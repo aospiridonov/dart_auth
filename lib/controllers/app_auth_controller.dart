703fe1591d0021153ae567944d5743a6ceffd7ca
aospiridonov <aospiridonov@gmail.com>
2022-06-13 12:34:18 +0300

    added model User and set requested with model data

--- lib/controllers/app_auth_controller.dart
+++ lib/controllers/app_auth_controller.dart
@@ -1,4 +1,5 @@
 import 'package:auth/models/response_model.dart';
+import 'package:auth/models/user.dart';
 import 'package:conduit/conduit.dart';
 
 class AppAuthController extends ResourceController {
@@ -7,30 +8,76 @@ class AppAuthController extends ResourceController {
   AppAuthController(this.managedContext);
 
   @Operation.post()
-  Future<Response> signIn() async => Response.ok(
-        ResponseModel(
-          data: {
-            'id': '1',
-            'refreshToken': 'refreshToken',
-            'accessToken': 'accessToken',
-          },
-          message: 'signIn ok',
-        ).toJson(),
-      );
+  Future<Response> signIn(@Bind.body() User user) async {
+    if (user.password == null || user.username == null) {
+      return Response.badRequest(
+          body: ResponseModel(
+        message: "Filds username and password are requiered!",
+      ));
+    }
+
+    final User fetchedUser = User();
+    //connect to DB
+    //find user
+    //check password
+    //fetch user
+
+    return Response.ok(
+      ResponseModel(
+        data: {
+          'id': fetchedUser.id,
+          'refreshToken': fetchedUser.refreshToken,
+          'accessToken': fetchedUser.accessToken,
+        },
+        message: 'Successful authorization',
+      ).toJson(),
+    );
+  }
 
   @Operation.put()
-  Future<Response> signUp() async => Response.ok(
-        ResponseModel(
-          data: {
-            'id': '1',
-            'refreshToken': 'refreshToken',
-            'accessToken': 'accessToken',
-          },
-          message: 'signUp ok',
-        ).toJson(),
-      );
+  Future<Response> signUp(@Bind.body() User user) async {
+    if (user.password == null || user.username == null || user.email == null) {
+      return Response.badRequest(
+          body: ResponseModel(
+        message: "Filds username, password and email are requiered!",
+      ));
+    }
+
+    final User fetchedUser = User();
+    //connect to DB
+    //create user
+    //fetch user
+
+    return Response.ok(
+      ResponseModel(
+        data: {
+          'id': fetchedUser.id,
+          'refreshToken': fetchedUser.refreshToken,
+          'accessToken': fetchedUser.accessToken,
+        },
+        message: 'Successful registration',
+      ).toJson(),
+    );
+  }
 
   @Operation.post('refresh')
-  Future<Response> refreshToken() async => Response.unauthorized(
-      body: ResponseModel(error: 'token is not valid').toJson());
+  Future<Response> refreshToken(
+      @Bind.path("refresh") String refreshToken) async {
+    final User fetchedUser = User();
+
+    //connect to DB
+    //find user
+    //check token
+    //fetch user
+    return Response.ok(
+      ResponseModel(
+        data: {
+          'id': fetchedUser.id,
+          'refreshToken': fetchedUser.refreshToken,
+          'accessToken': fetchedUser.accessToken,
+        },
+        message: 'Successful update tokens',
+      ).toJson(),
+    );
+  }
 }
